2bd52c4b443712144a012fc34e5939bbc868879c
Anabel Benitez Gonzalez <anabelbenitezglez.gmail.com>
2022-06-20 09:47:14 -0400

    Player strategies

---
+++ Player/Strategies.cs
@@ -0,0 +1,31 @@
+using Table;
+using Rules;
+using InfoGame;
+
+namespace Player;
+
+public interface IStrategy<T>
+{
+    public Jugada<T> Play(IList<Jugada<T>> PossiblePlays, GameStatus<T> status, InfoRules<T> rules);
+}
+
+public class RandomPlayer<T> : IStrategy<T>
+{
+    public Jugada<T> Play(IList<Jugada<T>> PossiblePlays, GameStatus<T> status, InfoRules<T> rules)
+    {
+        Random r = new Random();
+        int index = r.Next(0, PossiblePlays.Count());
+        return PossiblePlays[index];
+    }
+}
+
+public class GreedyPlayer<T> : IStrategy<T>
+{
+    public Jugada<T> Play(IList<Jugada<T>> PossiblePlays, GameStatus<T> status, InfoRules<T> rules)
+    {
+        Jugada<T> res = PossiblePlays[0];
+        for ( int i = 0; i < PossiblePlays.Count; i++ )
+            if(rules.ScoreToken.ScoreToken(res.Token)<rules.ScoreToken.ScoreToken(PossiblePlays[i].Token)) res = PossiblePlays[i]; 
+        return res;
+    }
+}
